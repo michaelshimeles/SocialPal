0baaa6613c9f529a65f1b112ca6b55b8a565baf2
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-29 00:25:00 -0400

    Fixed next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
                 hostname: '"utfs.io"',
                 port: '',
             },
+            {
+                protocol: 'https',
+                hostname: '"uploadthing.com"',
+                port: '',
+            },
         ],
 
     }
