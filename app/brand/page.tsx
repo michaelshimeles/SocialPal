3d019071d1afbf70e0b329014ed7c0e7c4289047
Michael Shimeles <michaelwasihun96@gmail.com>
2023-11-13 18:04:27 -0500

    Changed brand page with home button

--- app/brand/page.tsx
+++ app/brand/page.tsx
@@ -3,6 +3,7 @@ import { NavBar } from '@/components/NavBar';
 import Dashboard from '@/components/brand/dashboard';
 import { BrandCreate } from '@/components/brand/select/add-brand';
 import { SelectBrand } from '@/components/brand/select/select-brand';
+import { Button } from '@/components/ui/button';
 import {
     Dialog,
     DialogContent,
@@ -11,6 +12,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import Link from 'next/link';
 
 const DashboardPage = ({ }) => {
     return (
@@ -22,6 +24,11 @@ const DashboardPage = ({ }) => {
                         <DialogHeader>
                             <DialogTitle>Select or Create your Brand Profile</DialogTitle>
                             <div className='flex pt-7 justify-center gap-3'>
+                                <Link href="/">
+                                    <Button className='flex gap-2'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /><polyline points="9 22 9 12 15 12 15 22" /></svg>
+                                        Go Home</Button>
+                                </Link>
                                 <BrandCreate />
                                 <SelectBrand />
                             </div>
